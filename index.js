de58b97aef4b56db77821a91385f0ee4288cb8f0
Prachi Gupta <117148255+prachiguptadev@users.noreply.github.com>
2023-11-07 17:56:45 +0000

    change the function name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function multiplyThreeNumbers(num1, num2, num3) {
+function productThreeNumbers(num1, num2, num3) {
     return num1 * num2 * num3;
 }
 
-const result = multiplyThreeNumbers(2, 3, 4);
+const result = productThreeNumbers(2, 3, 4);
 console.log(result); 
